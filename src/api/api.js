7a3b91638b97277137c14c45d36614fa2579a1f6
吕晶048807 <lvjing@renmaitech.com>
2021-01-29 17:00:23 +0800

    feat:首次提交项目

---
+++ src/api/api.js
@@ -0,0 +1,14 @@
+import user from './user'
+import cases from "@/api/cases";
+import task from "@/api/task";
+import jenkins from "@/api/jenkins";
+import report from "@/api/report";
+
+const api = {
+    user,
+    cases,
+    task,
+    jenkins,
+    report
+}
+export default api
